87d0881ea0dd985780f4a86eb287b4cbb9929cd6
HOME <emmanuelbro090@gmail.com>
2024-07-21 21:41:25 +0300

    Final Commit

---
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.guessthenumber
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.guessthenumber.ui.GuessingGameScreen
+import com.example.guessthenumber.ui.theme.GuessTheNumberTheme
+import com.example.guessthenumber.ui.theme.MainViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GuessTheNumberTheme {
+                val viewModel = viewModel<MainViewModel>()
+              GuessingGameScreen(viewModel = viewModel )
+            }
+        }
+    }
+}
+
